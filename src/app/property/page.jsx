385dff73d0a609c3c40629ac87e0b7c42237a1eb
PriyanshuRawat24 <rawatpriyanshu653@gmail.com>
2025-02-25 15:50:38 +0530

    done all pages

--- src/app/property/page.jsx
+++ src/app/property/page.jsx
@@ -1,47 +1,45 @@
-import React from "react";
-import Image from "next/image";
-import Cards from "@/components/cards";
-import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbList, BreadcrumbPage, BreadcrumbSeparator } from "@/components/ui/breadcrumb";
+import Prop from '@/components/prop'
+import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbList, BreadcrumbPage, BreadcrumbSeparator } from '@/components/ui/breadcrumb'
+import React from 'react'
 
 const Page = () => {
   return (
-    <>
+    <div>
     {/* banner */}
-          <div className=" relative bg-blue-100 py-16 md:py-20 flex flex-col items-center text-center w-full">
-            {/* Background Overlay */}
-            <div
-              className="absolute inset-0 bg-cover bg-center opacity-30"
-              style={{ backgroundImage: "url('/pattern.png')" }}
-            ></div>
-    
-            {/* Content */}
-            <div className="relative z-10 px-4 md:px-8">
-              <h1 className="text-3xl md:text-5xl font-bold text-gray-800">
-                Property
-              </h1>
-              <Breadcrumb className="mt-6">
-                <BreadcrumbList className="text-gray-600 text-sm md:text-base mt-2">
-                  <BreadcrumbItem>
-                    <BreadcrumbLink href="/">Home</BreadcrumbLink>
-                  </BreadcrumbItem>
-                  <BreadcrumbSeparator />
-                  <BreadcrumbItem>
-                    <BreadcrumbPage>About Us</BreadcrumbPage>
-                  </BreadcrumbItem>
-                  <BreadcrumbSeparator />
-                  <BreadcrumbItem>
-                    <BreadcrumbPage>Property</BreadcrumbPage>
-                  </BreadcrumbItem>
-                </BreadcrumbList>
-              </Breadcrumb>
-            </div>
-          </div>
-    
-      <div className="grid grid-cols-3 place-items-center w-full border pl-6">
-        <Cards/>
-      </div>
-    </>
-  );
-};
+                  <div className=" relative bg-blue-100 py-16 md:py-20 flex flex-col items-center text-center w-full">
+                    {/* Background Overlay */}
+                    <div
+                      className="absolute inset-0 bg-cover bg-center opacity-30"
+                      style={{ backgroundImage: "url('/pattern.png')" }}
+                    ></div>
+            
+                    {/* Content */}
+                    <div className="relative z-10 px-4 md:px-8">
+                      <h1 className="text-3xl md:text-5xl font-bold text-gray-800">
+                        Contact
+                      </h1>
+                      <Breadcrumb className="mt-6">
+                        <BreadcrumbList className="text-gray-600 text-sm md:text-base mt-2">
+                          <BreadcrumbItem>
+                            <BreadcrumbLink href="/">Home</BreadcrumbLink>
+                          </BreadcrumbItem>
+                          <BreadcrumbSeparator />
+                          <BreadcrumbItem>
+                            <BreadcrumbPage>About Us</BreadcrumbPage>
+                          </BreadcrumbItem>
+                          <BreadcrumbSeparator />
+                          <BreadcrumbItem>
+                            <BreadcrumbPage>Property</BreadcrumbPage>
+                          </BreadcrumbItem>
+                        </BreadcrumbList>
+                      </Breadcrumb>
+                    </div>
+                  </div>
 
-export default Page;
+                  {/* section */}
+      <Prop/>
+    </div>
+  )
+}
+
+export default Page
